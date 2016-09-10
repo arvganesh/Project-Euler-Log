b3a95408aeb31c01adaba2d4a30c02cbfb068344
Arvind Ganesh <arvganesh@users.noreply.github.com>
2016-09-10 21:33:20 +0530

    Solved Problem 23.

--- Problem23.java
+++ Problem23.java
@@ -3,6 +3,8 @@ package problem23;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
+
 /**
  *
  * @author arvganesh
@@ -26,41 +28,51 @@ public class Problem23 {
         }
         return false;
     }
-    public static ArrayList getAbunNums() {
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 1; i < 28123; i++) {
-            if (isAbundantNum(i)) list.add(i);
+
+    public static Set<Integer> getAbunNums() {
+        HashSet<Integer> hs = new HashSet<>();
+        for (int i = 1; i <= 28123; i++) {
+            if (isAbundantNum(i)) {
+                hs.add(i);
+            }
         }
-        return list;
+        return hs;
     }
-    public static long getAllNums() {
-        int sum = 0;
-        for (int i = 1; i < 28123; i++) {
-            sum = sum + i;
-        }
+
+    public static long getTotalSum() {
+        int sum = (28123 * 28124) / 2;
         return sum;
     }
+
     public static void main(String[] args) {
         long sum = 0;
         HashSet<Long> hs = new HashSet<Long>();
+        HashSet<Integer> abunSet = new HashSet<Integer>(getAbunNums());
         int listSize = getAbunNums().size();
-        for (int i = 1; i < listSize; i++) {
-            for (int j = 1; j < listSize; j++) {
-                long ij = i+j;
+        for (int i = 1; i < 28123; i++) {
+            for (int j = 1; j < 28123; j++) {
+                long ij = i + j;
                 if (i + j > 28123) {
                     break;
                 }
-                if (isAbundantNum(i) && isAbundantNum(j)) hs.add(ij);
-                
+                if (abunSet.contains(i) && abunSet.contains(j)) {
+                    hs.add(ij);
+                }
+
             }
         }
-        System.out.println(hs);
+        //System.out.println(hs);
+
+        // Here on is good :D
         Iterator it;
         it = hs.iterator();
         while (it.hasNext()) {
             sum = sum + (long) it.next();
         }
-        System.out.println(sum);
+
+        // What you want
+        long ans = getTotalSum() - sum;
+        System.out.println(ans);
 
     }
 
