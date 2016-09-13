0edb6b50bfdb9602ce7acbcd5b311deaeb8e51d8
Arvind Ganesh <arvganesh@users.noreply.github.com>
2016-09-13 23:31:53 +0530

    Solved Problem 42

---
+++ Problem42.py
@@ -0,0 +1,27 @@
+def getTriangleNum(n): # Nth Triangular Number.
+    lst = []
+    for i in xrange(1, n + 1):
+         trinum = (i*(i+1))/2
+         lst.append(trinum)
+    return lst
+
+
+input_file = open("words.txt") # Reads File "words.txt", from project directory.
+text = input_file.read()
+text.replace('','') # replaces the commas.
+words = text.split(',') # Removes them.
+triangleList = getTriangleNum(100) # Generates 100 Triangular Numbers
+
+def toAscii(s): # Converts word to number for example, "A" is one and "B" is two and so on. The function adds up all of them in the word. For example, "ABCD" returns 10.
+    number = 0
+    for character in s:
+        number += ord(character) - 64
+    return number
+
+count = 0 # Number of Triangular Numbers
+for word in words:
+    w = word[1:-1] # Removes Quotations Around the words in the file.
+    number = toAscii(w) # Generates the number for the word.
+    if number in triangleList: # Checks if its a triangle number
+        count += 1 # increases count by one.
+print count # Prints out answer :D
