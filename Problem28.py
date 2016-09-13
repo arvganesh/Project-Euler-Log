1e9c7038b23b142c49ac75232da18faceb7ff95d
Arvind Ganesh <arvganesh@users.noreply.github.com>
2016-09-13 16:55:53 +0530

    Update Problem28.py

--- Problem28.py
+++ Problem28.py
@@ -1,16 +1,16 @@
-def diagonalNum(n):
+def diagonalNum(n): # n is the number of rows.
     increment = 2
     getDiaNum = 1
     sumOfNums = 0
     count = 1
-    limit = (n-1)/2
-    for i in xrange(1, limit + 1):
-        while count <= 4:
-            getDiaNum += increment
-            sumOfNums += getDiaNum
+    limit = (n-1)/2 # How Many Squares(excluding the first one) are added before answer. ex 5 rows : 2 squares, 7 : 3 etc.
+    for i in xrange(1, limit + 1): # 1 all the way until the limit
+        while count <= 4: # Four Numbers Per square
+            getDiaNum += increment # Each Number in the square(1,3,5,7,9,13 etc)
+            sumOfNums += getDiaNum # Sum of total numbers checked.
             count += 1             
-        increment += 2
-        count = 1
-    return sumOfNums + 1
+        increment += 2 # Every four numbers, count increases by two.
+        count = 1 # Resetting the value of count.
+    return sumOfNums + 1 # Didn't count the 1st box(which had "1")
 
-print diagonalNum(1001)
+print diagonalNum(1001) 
