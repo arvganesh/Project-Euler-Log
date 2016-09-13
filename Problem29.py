b64b82fbc4459e077693b5acb4e42e6d73c9251e
Arvind Ganesh <arvganesh@users.noreply.github.com>
2016-09-13 16:53:02 +0530

    Update Problem29.py

--- Problem29.py
+++ Problem29.py
@@ -1,5 +1,5 @@
-distinct_powers = set()
+distinct_powers = set() # Can't contain duplicates.
 for a in xrange(2, 101):
     for b in xrange(2, 101):
         distinct_powers.add(a**b)
-print len(distinct_powers)
+print len(distinct_powers) # Number Of Combinations
