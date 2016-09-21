cad1df0cb22ee35bca092240299bb4e53d3ce5f3
Arvind Ganesh <arvganesh@users.noreply.github.com>
2016-09-21 23:48:26 +0530

    Solved Problem 53.

---
+++ Problem53.py
@@ -0,0 +1,14 @@
+import math
+def nCr(n,r):
+    f = math.factorial
+    return f(n) / f(r) / f(n-r)
+
+r = 1
+count = 0
+
+for n in xrange(1, 101):
+    for r in xrange(1, n):
+        var = nCr(n, r)
+        if var > 1000000:
+            count += 1
+print count
