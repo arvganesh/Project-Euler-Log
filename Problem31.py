48e57c00fed68e59ec78ae7cf15f3e753e83d9c0
Arvind Ganesh <arvganesh@users.noreply.github.com>
2016-09-22 17:24:26 +0530

    Add files via upload

--- Problem31.py
+++ Problem31.py
@@ -1,12 +1,7 @@
-
-ways = 0
-target = 200
-for a in xrange(target, -1, -200):
-    for b in xrange(a, -1, -100):
-        for c in xrange(b, -1, -50):
-            for d in xrange(c, -1, -20):
-                for e in xrange(d, -1, -10):
-                    for f in xrange(e, -1, -5):
-                        for g in xrange(f, -1, -2):
-                            ways += 1
-print ways
+d = 1
+for i in range(1, 10):
+    for j in range(1, i):
+        q, r = divmod(9*j*i, 10*j-i)
+        if not r and q <= 9:
+            d *= i/float(j)
+print d
