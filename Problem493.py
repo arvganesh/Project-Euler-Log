c8781e5473efc113ed828ba4f6d94ad6a0486bb4
Arvind Ganesh <arvganesh@users.noreply.github.com>
2016-09-25 15:41:25 +0530

    Update Problem493.py

--- Problem493.py
+++ Problem493.py
@@ -1,9 +1,5 @@
-import math
-
+from math import factorial
 def nCr(n,r):
-    f = math.factorial
+    f = factorial
     return float(f(n) / f(r) / f(n-r))
-
-
-
 print str(float(7 * (1 - (nCr(60,20)) / (nCr(70,20)))))[:11]
