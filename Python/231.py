36276cdb6343a02c468fae41467ee4d7429c0f52
Arvind Ganesh <arvind.cganesh@gmail.com>
2016-10-08 23:09:39 +0530

    Added Text!

--- Python/231.py
+++ Python/231.py
@@ -1,8 +1,8 @@
 from Euler import prime_sieve
 n, r, s = 20000000, 15000000, 0
 for p in prime_sieve(n):
-    x = p
-    while x <= n:
-        s += p * (n//x - r//x - (n - r)//x) # Floor Division, exponenet number.
-        x *= p # Increasing the exponent.
+    j = 1
+    while p ** j <= n:
+        s += p * (n//p**j - r//p**j - (n - r)//p**j) # Floor Division, exponenet number.
+        j += 1 # Increasing the exponent.
 print s
